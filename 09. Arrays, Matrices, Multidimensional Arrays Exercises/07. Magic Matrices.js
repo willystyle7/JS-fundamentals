a505938c86d7cb0c85ee6327b2099bdd4d50f2b5
willystyle7 <ilia111@yahoo.com>
2018-07-06 17:49:18 +0300

    correct entry

--- 09. Arrays, Matrices, Multidimensional Arrays Exercises/07. Magic Matrices.js
+++ 09. Arrays, Matrices, Multidimensional Arrays Exercises/07. Magic Matrices.js
@@ -1,24 +1,36 @@
 function magicMatrices(input) {
-    let matrix = input.map(row => row.split(" ").map(Number));
-    let sum = matrix[0].reduce((a,b) => a+b);
+    let matrix = input.map(row => row.map(Number));
+    let sum = matrix[0].reduce((a, b) => a + b);
     let isMagic = true;
 
-    for(let i=1; i<matrix.length; i++) {
-        if(sum != matrix[i].reduce((a,b) => a+b)) {
+    for (let i = 1; i < matrix.length; i++) {
+        if (sum != matrix[i].reduce((a, b) => a + b)) {
             isMagic = false;
         }
     }
 
-    for(let col=0; col<matrix[0].length; col++) {
+    for (let col = 0; col < matrix[0].length; col++) {
         let sumCol = 0;
-        for(let row=0; row<matrix.length; row++) {
+        for (let row = 0; row < matrix.length; row++) {
             sumCol += matrix[row][col];
         }
 
-        if(sumCol != sum) {
+        if (sumCol != sum) {
             isMagic = false;
         }
     }
 
     console.log(isMagic);
 }
+
+magicMatrices([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]);
+console.log();
+magicMatrices([[11, 32, 45],
+    [21, 0, 1],
+    [21, 1, 1]]);
+console.log();
+magicMatrices([[1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0]]);
